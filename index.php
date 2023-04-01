9e7941517029668d3405c458ad5b571bb0e1a7aa
hlavka <milkz@seznam.cz>
2023-04-01 10:02:17 +0200

    přidání receptů

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Domů</title>
+    <link rel="stylesheet" href="css.css">
+</head>
+<body style="background-color: rgb(185, 214, 242);">
+    <ul>
+        <li><a class="active" href="HlavniStrana.html">Domů</a></li>
+        <li><a href="">Recepty</a></li>
+        <li><a href="">Oblíbené</a></li>
+        <li><a href="">Na později</a></li>
+        <li style="float:right"><a href="">Přihlásit</a></li>
+      </ul>
+      <div id="Doporucujeme"><h1 >Doporučujeme</h1></div>
+      
+      <div id="doporuceno">
+    
+      </div>
 
+
+
+</body>
+</html>
