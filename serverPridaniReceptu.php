6cfaadfd19493cb87fd004f854c47c8adf55dcdd
hlavka <milkz@seznam.cz>
2023-04-01 12:01:09 +0200

    přidávání receptů funkční

--- serverPridaniReceptu.php
+++ serverPridaniReceptu.php
@@ -2,14 +2,20 @@
 
 $nazev = $_POST["nazev"];
 $doba = $_POST["doba"];
-$doba += " min";
+
 $postup = $_POST["postup"];
 $nar = $_POST["nar"];
 
 $connect = new mysqli("localhost","root","","receptar");
 
 
-$sql ="";
+$sql ="INSERT into recepty(nazev,postup,dobaPripravy,narocnost) VALUES('$nazev','$postup','$doba min','$nar')";
+
+if($connect->query($sql)){
+    echo "Úspěšně uloženo";
+}
+header('location:index.php');
+die();
 
 
 
