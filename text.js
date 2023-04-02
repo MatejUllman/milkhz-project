e2074cbd4d5bf14a3fa949f15156e9d50c36b545
hlavka <milkz@seznam.cz>
2023-04-02 15:39:54 +0200

    funguje

--- text.js
+++ text.js
@@ -4,6 +4,6 @@
 function vypis(id){
     let element = document.getElementById(id).value;
     let el = "<p>"+element+"</p>";
-    document.getElementById("neco").appendChild(el);
-    console.log(element);
+    document.getElementById("neco").innerHTML = el;
+    
 }
