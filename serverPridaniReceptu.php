844a253157eee9547dac8f1baef5732b805ec37b
hlavka <milkz@seznam.cz>
2023-04-02 10:22:26 +0200

    přidán obrázek

--- serverPridaniReceptu.php
+++ serverPridaniReceptu.php
@@ -5,11 +5,11 @@ $doba = $_POST["doba"];
 
 $postup = $_POST["postup"];
 $nar = $_POST["nar"];
-
+$obr = $_POST["obr"];
 $connect = new mysqli("localhost","root","","receptar");
 
 
-$sql ="INSERT into recepty(nazev,postup,dobaPripravy,narocnost) VALUES('$nazev','$postup','$doba min','$nar')";
+$sql ="INSERT into recepty(nazev,postup,dobaPripravy,narocnost,obrazek) VALUES('$nazev','$postup','$doba min','$nar','$obr')";
 
 if($connect->query($sql)){
     echo "Úspěšně uloženo";
