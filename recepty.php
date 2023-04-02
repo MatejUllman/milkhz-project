dce5895110a3d1acead179f28711411c9b465547
hlavka <milkz@seznam.cz>
2023-04-02 13:01:27 +0200

    vypisování

--- recepty.php
+++ recepty.php
@@ -17,8 +17,45 @@
         <li style="float:right"><a href="">Přihlásit</a></li>
       </ul>
       <div id="recept"><h1 >Recepty</h1></div>
-      
-      <div style="margin-left: 5%; display: inline-block;" id="doporuceno"></div>
-        <div style="float: right; margin-right: 5%;" id="doporuceno"><h2>Zde se objeví postu přípravy receptu</h2></div>
+<div style="display:flex;">
+      <div style="margin-left: 5%; display: inline-block; display:flex;" id="doporuceno">
+      <?php
+     $connect = new mysqli("localhost","root","","receptar");
+
+     $sql = "SELECT * FROM recepty order by id asc;";
+     
+     $results = $connect->query($sql);
+     if($results == true){
+         
+        while($row = $results->fetch_object()){
+           
+            vypis($row);
+        }
+     }
+     function vypis($row){
+       // zde udělej css-ka
+        echo "<div>
+        <h1>$row->nazev</h1><br>
+        <!--<p>$row->postup</p>-->
+        <p>Náročnost: $row->narocnost</p>
+        <!-- style='margin-left:250px;' -->
+        </div>
+        <div >
+        <img src='$row->obrazek ' width='100px' height='100px'>
+        <p>doba přípravy: $row->dobaPripravy</p>
+        <button type='button'>zobrazit postup</button> 
+        </div>
+        ";
+        
+     }
+        
+    ?>
+    
+    
+    </div>
+        <div style="float: right; margin-right: 5%;" id="doporuceno">
+        <h2>Zde se objeví postu přípravy receptu</h2>
+    </div>
+</div>
 </body>
 </html>
