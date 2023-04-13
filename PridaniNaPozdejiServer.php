cfa27b58155dc9d2f8aca578a6dff3b487f11d77
hlavka <milkz@seznam.cz>
2023-04-13 11:19:52 +0200

    přidávání do přečíst později

--- PridaniNaPozdejiServer.php
+++ PridaniNaPozdejiServer.php
@@ -1,8 +1,30 @@
 <?php
 
+$nazev = $_POST["nazev"];
+$doba = $_POST["dob"];
+$id = $_POST["id"];
+$postup = $_POST["postup"];
+$nar = $_POST["nar"];
+$obr = $_POST["obr"];
 
+echo $nazev;
+echo $doba;
+echo $obr;
+echo $nar;
+echo $postup;
+echo $id;
 
+$connect = new mysqli("localhost","root","","receptar");
 
-
-
+    $sql ="INSERT into odlozenerecepty(nazev,postup,dobaPripravy,narocnost,obrazek) VALUES('$nazev','$postup','$doba','$nar','$obr');";
+    //DELETE FROM recepty WHERE `recepty`.`id` = $id;
+    $sqldva = "DELETE FROM recepty WHERE id = $id;";
+    if($connect->query($sql)){
+        echo "Úspěšně uloženo";
+    }
+    if($connect->query($sqldva)){
+        echo "Úspěšně uloženo";
+    }
+    header('location:recepty.php');
+    die();
 ?>
